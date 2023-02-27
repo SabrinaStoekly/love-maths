7f5b1848a48aea14392677e5cfaa44eed9af9bf8
SabrinaStoekly <sstoekly@gmail.com>
2023-02-27 11:09:13 +0000

    add division code

--- assests/js/script.js
+++ assests/js/script.js
@@ -43,11 +43,12 @@ function runGame(gameType){
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract"){
         displaySubtractQuestion(num1, num2);
+    }  else if (gameType === "division"){
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unkown game type ${gameType}`);
         throw `Unkown  game type ${gameType}. Aborting!`;
     }
-    
 }
 
 /**
@@ -87,7 +88,9 @@ function calculateCorrectAnswer(){
         return [operand1  * operand2, "multiply"];
     } else if (operator === "-"){
         return [operand1 - operand2, "subtract"];
-    }  else{
+    }  else if (operator === "/") { 
+        return [operand1 / operand2, "division"];
+    } else{
         alert(`Uniplemented operator ${operator}`);
         throw  `Uniplemented operator ${operator}`;
     }
@@ -134,5 +137,14 @@ function displayMultiplyQuestion(operand1, operand2){
     document.getElementById('operand1').textContent =  operand1;
     document.getElementById('operand2').textContent =  operand2;
     document.getElementById('operator').textContent =  "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
+    
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
+ 
 }
      
